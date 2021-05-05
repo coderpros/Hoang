9c5811c8454aa878af8f8218f0a7c831b0e982cf
Brandon Osborne <brandon.osborne@coderpro.net>
2021-05-05 11:59:40 -0500

    Update Globals.vb

--- Hoang.FtpRename/Globals.vb
+++ Hoang.FtpRename/Globals.vb
@@ -2,5 +2,5 @@
 
     Public Dim g_FtpServer As String = "ftp.site4now.net/"
     Public Dim g_FtpUserName As String = "coderpro_test"
-    Public Dim g_FtpPassword As String = "Q1w2e3r4t5y6!"
+    Public Dim g_FtpPassword As String = "XXXXXXXXXXXXX!"
 End Module
